e9d71e8666e2b13a42bdac8b44d5ef80325ab44b
TeeNik <teenik3315@gmail.com>
2020-09-10 23:53:21 +0300

    copy movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float _jumpForce = 400f;
+    [Range(0, .3f)] [SerializeField] private float _movementSmoothing = .05f;
+    [SerializeField] private bool _airControl = false;
+    [SerializeField] private LayerMask _whatIsGround;
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private Transform _ceilingCheck;
+
+    const float _groundedRadius = .2f;
+    private bool _grounded;
+    const float _ceilingRadius = .2f;
+    private Rigidbody2D _rigidbody2D;
+    private bool _facingRight = true;
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _grounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_groundCheck.position, _groundedRadius, _whatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                _grounded = true;
+        }
+    }
+
+    public void Move(float move, bool jump)
+    {
+        if (_grounded || _airControl)
+        {
+            Vector3 targetVelocity = new Vector2(move * 10f, _rigidbody2D.velocity.y);
+            _rigidbody2D.velocity = Vector3.SmoothDamp(_rigidbody2D.velocity, targetVelocity, ref _velocity, _movementSmoothing);
+            if (move > 0 && !_facingRight || move < 0 && _facingRight)
+            {
+                Flip();
+            }
+        }
+        if (_grounded && jump)
+        {
+            _grounded = false;
+            _rigidbody2D.AddForce(new Vector2(0f, _jumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        _facingRight = !_facingRight;
+        Vector3 newScale = transform.localScale;
+        newScale.x *= -1;
+        transform.localScale = newScale;
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public CharacterController controller;
+    public CharacterController copyPrefab;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    private CharacterController _copy = null;
+    private float _time = 0;
+    private float _spawnTime = 0;
+
+    private enum Action
+    {
+        None,
+        Left,
+        Right
+    }
+
+    private Queue<KeyValuePair<float, float>> _history = new Queue<KeyValuePair<float, float>>();
+    private Dictionary<float, float> _actions = new Dictionary<float, float>();
+    private float _last = -1;
+
+
+    private bool _inUpdate = true;
+
+    void HandleInput()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        print(horizontalMove);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _inUpdate = !_inUpdate;
+            print("inUpdate: " + _inUpdate);
+
+        }
+
+        if (_last != horizontalMove)
+        {
+            _actions.Add(_time, horizontalMove);
+            _history.Enqueue(new KeyValuePair<float, float>(_time, horizontalMove));
+            _last = horizontalMove;
+            //print(_time + "  " + horizontalMove);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            var pos = transform.position;
+            pos.x = 0;
+            _copy = Instantiate(copyPrefab, pos, transform.rotation);
+            _spawnTime = _time;
+        }
+        _time += Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if (_inUpdate)
+        {
+            HandleInput();
+        }
+
+    }
+
+    private float _copySpeed = 0;
+    void FixedUpdate()
+    {
+        if (!_inUpdate)
+        {
+            HandleInput();
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+
+        if (_copy != null)
+        {
+            if (_history.Count > 0)
+            {
+                var first = _history.Peek();
+                if (first.Key <= _time - _spawnTime)
+                {
+                    _history.Dequeue();
+                    _copySpeed = first.Value;
+                }
+            }
+            _copy.Move(_copySpeed * Time.fixedDeltaTime, false);
+        }
+    }
+}
