53dc8843caa2b61096b8fb5acaefb7741c696fa3
TeeNik <teenik3315@gmail.com>
2020-09-15 02:17:27 +0300

    health component

---
+++ Assets/Scripts/Interface/HealthComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag(StringTags.Danger))
+        {
+            print("Die");
+            Destroy(gameObject);
+        }
+    }
+}
