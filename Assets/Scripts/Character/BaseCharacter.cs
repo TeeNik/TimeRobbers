112940440496bb40f0c059be250ca17dadd69ee7
TeeNik <teenik3315@gmail.com>
2020-09-18 02:20:11 +0300

    time scale component

--- Assets/Scripts/Character/BaseCharacter.cs
+++ Assets/Scripts/Character/BaseCharacter.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseCharacter : MonoBehaviour, IMortal
+public class BaseCharacter : MonoBehaviour
 {
+
+    [SerializeField] private CharacterController _characterController;
+    [SerializeField] private InputController _inputController;
+
+    private HealthComponent _health;
+
     void Start()
     {
-
+        _health = gameObject.AddComponent<HealthComponent>();
     }
 
 }
---
+++ Assets/Scripts/Interface/TimeSpeedComponent.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class TimeSpeedComponent : MonoBehaviour
+{
+    public float TimeScale = 1.0f;
+}
