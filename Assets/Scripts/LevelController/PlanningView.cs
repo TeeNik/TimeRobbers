501a2fd89328ee2292dc74f0ae78398c0831399f
TeeNik <teenik3315@gmail.com>
2020-09-20 23:24:21 +0300

    planning stage [2]

---
+++ Assets/Scripts/LevelController/PlanningView.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class PlanningView : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text _numOfRecords;
+
+    public void SetNumberOfRecords(int count)
+    {
+        _numOfRecords.text = "Records: " + count;
+    }
+
+}
---
+++ Assets/Scripts/LevelController/StageManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+
+    public enum StageType
+    {
+        Planning,
+        Heist,
+    }
+
+
+}
