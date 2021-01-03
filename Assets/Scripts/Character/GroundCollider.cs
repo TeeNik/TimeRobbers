abd6cf78311c95a1c7bd21f6b9eab753993e62f6
TeeNik <teenik3315@gmail.com>
2021-01-04 02:44:58 +0300

    ground collider

---
+++ Assets/Scripts/Character/GroundCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class GroundCollider : MonoBehaviour
+{
+    public LayerMask WhatIsGround;
+
+    private CircleCollider2D _collider;
+
+    public bool IsTouchingGround
+    {
+        get { return _collider.IsTouchingLayers(WhatIsGround); }
+    }
+
+    void Awake()
+    {
+        _collider = GetComponent<CircleCollider2D>();
+    }
+
+}
